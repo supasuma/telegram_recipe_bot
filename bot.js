47f4660f00dac0464ae91f9b6b9eb49e1b7bae27
supasuma <supasuma@gmail.com>
2016-11-25 16:11:56 +0000

    got json and displayed

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-
+var fetch = require('node-fetch');
 var Bot = require('node-telegram-bot-api');
 var bot;
 
@@ -22,3 +22,29 @@ bot.on('message', function (msg) {
   var resp = "Recipe bot in action"
   bot.sendMessage(chatId, resp);
 })
+
+bot.onText(/^\/get_recipes (.+)$/, function (msg, match) {
+ var ingredients = match[1].split(" ");
+ var url;
+ ingredients.map( (ingredient) => {
+  url = 'http://food2fork.com/api/search?key=ac1dade071c008a757442d968333ebc6&q=' + ingredient;
+})
+  var chatId = msg.chat.id;
+  var resp = ""
+
+  fetch(url)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        var recipe = getRecipes(json);
+        resp = "Recipes containing: " + ingredients + "\n" + recipe.title + "\n" + recipe.source_url;
+        console.log(resp)
+        bot.sendMessage (chatId, resp);
+    });
+})
+
+var getRecipes = function(json) {
+  // console.log(json.recipes[0].title);
+  var recipeTitle = json.recipes[0]
+  return recipeTitle
+}
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
   telegramCredentials: {
     token: "290389027:AAFV-J-iM8QzqoR2Yn8X1MJCAeqjRrfQ1UM"
+  },
+
+  food2fork: {
+    apiKey: "ac1dade071c008a757442d968333ebc6"
   }
 
 }
